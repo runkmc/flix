1da27a1660a469f994c120defb7253f46820883c
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-04-26 10:20:51 -0700

    create movie navigation

---
+++ spec/features/navigate_movies_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "Navigating movies" do
+  it "allows navigation from the detail page to the listing page" do
+    movie = Movie.create(movie_attributes)
+
+    visit movie_url(movie)
+    click_link "All Movies"
+    expect(current_path).to eq(movies_path)
+  end
+
+  it "allows navigation from the listing page to the detail page" do
+    movie = Movie.create(movie_attributes)
+
+    visit movies_url
+    click_link "Iron Man"
+    expect(current_path).to eq(movies_path(movie))
+  end
+end
