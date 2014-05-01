fb9184dd04e382c53802d95f76764ae59fa11a70
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-05-01 16:44:20 -0700

    Add update action for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,11 +5,24 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params[:id])
+    @movie = params_id
   end
 
   def edit
-    @movie = Movie.find(params[:id])
+    @movie = params_id
+  end
+
+  def update
+    @movie = params_id
+    movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
+  private
+
+  def params_id
+    Movie.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Flix::Application.routes.draw do
   get 'movies' => 'movies#index'
   get 'movies/:id' => 'movies#show', as: 'movie'
   get 'movies/:id/edit' => 'movies#edit', as: 'edit_movie'
+  patch "movies/:id" => "movies#update"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
