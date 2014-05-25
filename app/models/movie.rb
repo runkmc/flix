022f90f9306059f5c47bc39fe178c006604d9a97
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2014-05-25 16:03:43 -0700

    Fix typo

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ActiveRecord::Base
   RATINGS = %w(G PG PG-13 R NC-17)
 
   validates :title, :released_on, :duration, presence: true
-  validates :description, length: { minimun: 25 }
+  validates :description, length: { minimum: 25 }
   validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
   validates :image_file_name, allow_blank: true, format: {
     with:    /\w+.(gif|jpg|png)\z/i,
